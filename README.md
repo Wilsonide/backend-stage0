24fc5e22df84ee8f7972eceb896be8a33ee4b752
Wilson Icheku <ichekuwilson538@gmail.com>
2025-01-29 05:13:12 +0100

    commit changes

---
+++ README.md
@@ -0,0 +1,39 @@
+# Project documentation
+
+## Description
+
+this api returns the information of user registered in hng12.
+
+- Endpoint -  <http://localhost:8000/user>
+
+### Example Response
+
+```bash
+{
+    "email": "ichekuwilson538@gmail.com",
+    "current_datetime": 2025-01-29T03:30:16.073395+00:00,
+    "github_url": "https://github.com/Wilsonide",
+}
+```
+
+## Project setup
+
+```bash
+pip install -r requirements.txt
+```
+
+## Compile and run the project
+
+```bash
+python main.py
+
+```
+
+## Stay in touch
+
+- [https://hng.tech/hire/python-developers](https://hng.tech/hire/python-developers)
+- [https://hng.tech/hire/csharp-developers](https://hng.tech/hire/csharp-developers)
+- [https://hng.tech/hire/golang-developers](https://hng.tech/hire/golang-developers)
+- [https://hng.tech/hire/php-developers](https://hng.tech/hire/php-developers)
+- [https://hng.tech/hire/java-developers](https://hng.tech/hire/java-developers)
+- [https://hng.tech/hire/nodejs-developers](https://hng.tech/hire/nodejs-developers)
---
+++ main.py
@@ -0,0 +1,50 @@
+from fastapi import FastAPI,status
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+from datetime import datetime, timezone
+from pydantic import BaseModel
+
+class UserResponse(BaseModel):
+    email: str
+    current_datetime: str
+    github_url: str
+    class Config:
+        orm_mode = True
+
+
+def init():
+    async def lifespan(app:FastAPI):
+        yield
+    app = FastAPI(title='Fastapi',lifespan=lifespan)
+    return app
+
+app = init()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["*"],
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ['*']
+)
+
+
+@app.get('/user',status_code=status.HTTP_200_OK,response_model=UserResponse)
+def get_user():
+    # Get the current date and time in UTC
+    current_datetime_utc = datetime.now(timezone.utc)
+
+    # Format the datetime in ISO 8601 format
+    iso_8601_utc = current_datetime_utc.isoformat()
+    return {
+        "email": "ichekuwilson538@gmail.com",
+        "current_datetime": iso_8601_utc,
+        "github_url": "https://github.com/Wilsonide",
+
+        }
+
+
+
+
+if __name__ == "__main__":
+    uvicorn.run('main:app',reload=True,port=8000)
